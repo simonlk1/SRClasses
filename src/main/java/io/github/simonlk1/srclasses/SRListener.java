b65061ab15c3d94562c1051401b1451991bd48ea
BuildTools <unconfigured@null.spigotmc.org>
2021-03-27 16:27:43 +0100

    first commit

---
+++ src/main/java/io/github/simonlk1/srclasses/SRListener.java
@@ -0,0 +1,12 @@
+package io.github.simonlk1.srclasses;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class SRListener implements Listener {
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event){
+        //do something
+    }
+}
